8b1f71e1588b4e51c5eb3791c9f1d69e02266aaa
Jie <lijie121210@163.com>
2016-04-29 10:51:10 +0800

    Initial Commit

---
+++ JHSwitch/JHSwitch.h
@@ -0,0 +1,19 @@
+//
+//  JHSwitch.h
+//  JHSwitch
+//
+//  Created by Lijie on 16/4/29.
+//  Copyright © 2016年 HuatengIOT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JHSwitch.
+FOUNDATION_EXPORT double JHSwitchVersionNumber;
+
+//! Project version string for JHSwitch.
+FOUNDATION_EXPORT const unsigned char JHSwitchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JHSwitch/PublicHeader.h>
+
+
